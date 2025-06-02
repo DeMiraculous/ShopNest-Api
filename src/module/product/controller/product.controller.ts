c9c3a6a2cb18967577e52df15aace1db3b1e5ad8
Demiraculous <otokinam@gmail.com>
2025-06-02 12:59:32 +0100

    product photo upload implementation

--- src/module/product/controller/product.controller.ts
+++ src/module/product/controller/product.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Get, Patch, Delete, Param, Body, Query } from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Param, Body, Query, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { CreateProductDto } from '../dto/product.dto';
 import { ProductService } from '../service/product.service';
 import { PaginatedRecordsDto, PaginationDto } from 'src/module/common/dto/paginated.dto';
 import { LoggedInUser } from 'src/module/auth/decorators/loggedInUser.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 
 @Controller('products')
@@ -14,10 +16,34 @@ export class ProductController {
    * @returns 
    */
   @Post()
+  @UseInterceptors(FileInterceptor("image"))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        description: { type: 'string' },
+        price: { type: 'number' },
+        stock: { type: 'integer' },
+        category: { type: 'string' },
+        vendorId: { type: 'string' },
+        image: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   createProduct(
     @LoggedInUser("id") userId: string,
-    @Body() createProductDto: CreateProductDto) {
-    return this.productsService.createProduct(userId, createProductDto);
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    return this.productsService.createProduct(
+      userId,
+      createProductDto,
+      file);
   }
 
   /**
@@ -39,19 +65,19 @@ export class ProductController {
   getProductById(@Param('id') id: string) {
     return this.productsService.getProductById(id);
   }
- /** update a product by it's id
- * @param id
- * @returns 
- */
+  /** update a product by it's id
+  * @param id
+  * @returns 
+  */
   @Patch(':id')
   updateProduct(@Param('id') id: string, @Body() data: Partial<CreateProductDto>) {
     return this.productsService.updateProduct(id, data);
   }
-    /**
- * delete a product by it's id
- * @param id
- * @returns 
- */
+  /**
+* delete a product by it's id
+* @param id
+* @returns 
+*/
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
     return this.productsService.deleteProduct(id);
--- src/module/product/service/product.service.ts
+++ src/module/product/service/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger, NotFoundException, Query } from "@nestjs/common";
 import { ProductRepository } from "../repository/product.repository";
 import { CreateProductDto } from "../dto/product.dto";
+import { v2 as cloudinary } from 'cloudinary';
+import toStream from 'streamifier';
 import { PaginatedRecordsDto, PaginationDto } from "src/module/common/dto/paginated.dto";
 
 @Injectable()
@@ -17,8 +19,36 @@ export class ProductService {
    * @param createProductDto create a product
    * @returns 
    */
-  async createProduct(userId: string, data: CreateProductDto) {
+  async createProduct(
+    userId: string,
+    data: CreateProductDto,
+    file: Express.Multer.File
+  ) {
+    let imageUrl = data.imageUrl;
+    if (file) {
+      imageUrl = await this.uploadToCloudinary(file);
+    }
+
     return this.productRepository.createProduct(userId, data);
+  }
+  /**
+   * function to upload a file to cloudinary
+   * @returns the url of the uploaded image
+   */
+  async uploadToCloudinary(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: 'products' },
+        (error, result) => {
+          if (error) return reject(error);
+          if (!result || !result.secure_url) {
+            return reject(new Error("Cloudinary upload failed or result is undefined"));
+          }
+          resolve(result.secure_url);
+        }
+      );
+      toStream(file.buffer).pipe(uploadStream); //converts buffer provided by mutler to a readable stream needed by cloudinary
+    });
   }
   /**
    * get all product
@@ -39,18 +69,18 @@ export class ProductService {
     if (!product) throw new NotFoundException('Product not found');
     return product;
   }
- /** update a product by it's id
- * @param id
- * @returns 
- */
+  /** update a product by it's id
+  * @param id
+  * @returns 
+  */
   async updateProduct(id: string, data: Partial<CreateProductDto>) {
     return this.productRepository.updateProduct(id, data);
   }
-    /**
- * delete a product by it's id
- * @param id
- * @returns 
- */
+  /**
+* delete a product by it's id
+* @param id
+* @returns 
+*/
   async deleteProduct(id: string) {
     return this.productRepository.deleteProduct(id);
   }
